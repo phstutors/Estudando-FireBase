50b2a8073f7f6fd83c48226bdd6f834f4d2f2d85
phstutors <phsefamilia8@gmail.com>
2024-08-13 16:54:10 -0300

    Aprendendo Login, logout, persistencia e exclusao de conta

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ['*.html'],  // Inclua o caminho correto para os seus arquivos HTML
+  darkMode: 'class', // Modo escuro baseado em classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
